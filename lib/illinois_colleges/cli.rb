522f8c0af2d7ca042c0a791bf8833c051f5e3508
Daniel Etter <etter.dt@gmail.com>
2018-09-25 22:41:07 +0000

    modified CLI

--- lib/illinois_colleges/cli.rb
+++ lib/illinois_colleges/cli.rb
@@ -3,10 +3,13 @@
 class IllinoisColleges::CLI 
   
   def call
-    puts "Colleges and universities in the state of Illinois:"
+    list_colleges
+    menu
+    goodbye
   end
   
   def list_colleges
+    puts "Colleges and universities in the state of Illinois:"
     puts <<~DOC
       1. Elmhurst College
       2. Illinois State University
@@ -14,5 +17,27 @@ class IllinoisColleges::CLI
       4. University of Chicago
       5. University of Illinois
     DOC
+  end
+  
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Please enter the number of the institution that you would like more information on, type 'list' to list the institutions again, or type 'exit':"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More info on Elmhurst College..."
+      when "2"
+        puts "More info on Illinois State University..."
+      when "list"
+        list_colleges
+      else
+        puts "Not sure what you meant, please type 'list' or 'exit'.."
+      end
+    end
+  end
+  
+  def goodbye
+    puts "Come back anytime to learn more about the colleges and universities in the state of Illinois!"
   end
 end
