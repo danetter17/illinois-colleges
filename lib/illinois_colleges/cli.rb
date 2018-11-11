1bc33b3455561a81c113f00b04988e0ff8ebef9e
Daniel Etter <etter.dt@gmail.com>
2018-11-11 17:27:40 +0000

    Completed functionality of the cli

--- lib/illinois_colleges/cli.rb
+++ lib/illinois_colleges/cli.rb
@@ -3,16 +3,35 @@
 class CLI 
   
   def call
+    welcome
     list_colleges
     menu
     goodbye
+  end
+  
+  def welcome
+    input = nil
+    while input != "exit"
+      puts "Welcome, to see a list of colleges and universities in the state of Illinois type 'list', or type 'exit' to exit the program:"
+      input = gets.strip.downcase
+      
+      if input == "list"
+        list_colleges
+        menu
+      elsif input == "exit"
+        goodbye
+        exit
+      else
+        puts "Not sure what you meant, please type 'list' or 'exit'.."
+      end
+    end
   end
   
   def list_colleges
     puts "Colleges and universities in the state of Illinois:"
     @colleges = College.college
     @colleges.drop(1).each.with_index(1) do |college, i|
-      puts "#{i}. #{college.name} - #{college.location}"
+      puts "#{i}. #{college.name}"
     end
   end
   
@@ -24,9 +43,12 @@ class CLI
       
       if input.to_i > 0 
         the_college = @colleges[input.to_i]
-        puts "#{the_college.name} - #{the_college.location} - #{the_college.size}"
+        puts "#{the_college.name} - #{the_college.location}"
       elsif input == "list"
         list_colleges
+      elsif input == "exit"
+        goodbye
+        exit
       else
         puts "Not sure what you meant, please type 'list' or 'exit'.."
       end
